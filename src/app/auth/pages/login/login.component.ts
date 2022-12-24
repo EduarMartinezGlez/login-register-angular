69a05ba67066f45203331e4d4607ec5dc99fc489
EduarMartinezGlez <emartinezgonzalez2012@gmail.com>
2022-12-24 00:10:29 -0500

    night 23

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
 import { environment } from 'src/environments/environment';
 
@@ -11,13 +11,24 @@ import { environment } from 'src/environments/environment';
 export class LoginComponent implements OnInit{
 
   siteKey:string
-  constructor(private recaptchaService:ReCaptchaV3Service){
+  constructor(
+    private recaptchaService:ReCaptchaV3Service,
+    private fb:FormBuilder
+    ){
     this.siteKey = environment.siteKey
   }
-
-
-
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+
+  loginForm: FormGroup= this.fb.group({
+    email: ['',[
+      Validators.required,
+      Validators.email,
+      Validators.pattern(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/)
+     ]],
+    password:['',[ Validators.pattern(
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/
+      )]]
+  })
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { recaptcha } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private fb:FormBuilder,
+    private router:Router){}
+
+  registerForm:FormGroup=this.fb.group({
+    name: ['',[Validators.required]],
+    email:['', [Validators.required,Validators.email,Validators.pattern(/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/)]],
+    password:['',[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/)]],
+    recaptcha:['',[Validators.required]]
+  })
+
+
+
+register(){
+console.log(this.registerForm.value);
+this.router.navigateByUrl('/dashboard')
+
+}
 }
